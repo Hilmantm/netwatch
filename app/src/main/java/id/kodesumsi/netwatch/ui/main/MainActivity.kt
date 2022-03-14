e3b445b0b46bfd0d3c08ae2475c70a062cc6ecac
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-14 21:22:21 +0700

    Add utility function in MainActivity.kt for showLoading, showRvContent and return image resource TMDB API

--- app/src/main/java/id/kodesumsi/netwatch/ui/main/MainActivity.kt
+++ app/src/main/java/id/kodesumsi/netwatch/ui/main/MainActivity.kt
@@ -3,8 +3,11 @@ package id.kodesumsi.netwatch.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.ProgressBar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import id.kodesumsi.netwatch.R
 import id.kodesumsi.netwatch.base.BaseActivity
@@ -18,5 +21,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun setupViewInstance(savedInstanceState: Bundle?) {
         val navController = findNavController(R.id.main_fragment_container_view)
         binding.bnvMain.setupWithNavController(navController)
+    }
+
+    companion object {
+        fun showLoading(pb: ProgressBar, rv: RecyclerView) {
+            pb.visibility = View.VISIBLE
+            rv.visibility = View.INVISIBLE
+        }
+
+        fun showRvContent(pb: ProgressBar, rv: RecyclerView) {
+            pb.visibility = View.GONE
+            rv.visibility = View.VISIBLE
+        }
+
+        fun imageResource(path: String): String {
+            return "https://image.tmdb.org/t/p/w500/$path"
+        }
     }
 }
