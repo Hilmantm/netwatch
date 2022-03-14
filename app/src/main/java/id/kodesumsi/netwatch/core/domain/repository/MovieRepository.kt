0a2aa4aa1249f38da809670e3678e7b4948433c5
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-14 21:27:37 +0700

    Change repository function to return flowable resource list of movie

--- app/src/main/java/id/kodesumsi/netwatch/core/domain/repository/MovieRepository.kt
+++ app/src/main/java/id/kodesumsi/netwatch/core/domain/repository/MovieRepository.kt
@@ -1,5 +1,9 @@
 package id.kodesumsi.netwatch.core.domain.repository
 
+import id.kodesumsi.netwatch.core.data.source.Resource
+import id.kodesumsi.netwatch.core.domain.model.Movie
+import io.reactivex.rxjava3.core.Flowable
+
 interface MovieRepository {
-    fun getMovieList(category: String)
+    fun getMovieList(category: String): Flowable<Resource<List<Movie>>>
 }
