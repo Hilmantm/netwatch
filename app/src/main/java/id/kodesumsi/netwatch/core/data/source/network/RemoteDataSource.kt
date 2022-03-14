8ece4b244d997f50fee285e4c6664db455cf6cfe
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-14 21:26:06 +0700

    Refactor Movie data class into MovieResponse data class

--- app/src/main/java/id/kodesumsi/netwatch/core/data/source/network/RemoteDataSource.kt
+++ app/src/main/java/id/kodesumsi/netwatch/core/data/source/network/RemoteDataSource.kt
@@ -1,8 +1,8 @@
 package id.kodesumsi.netwatch.core.data.source.network
 
-import id.kodesumsi.netwatch.core.data.source.network.response.Movie
+import id.kodesumsi.netwatch.core.data.source.network.response.MovieResponse
 import io.reactivex.rxjava3.core.Flowable
 
 interface RemoteDataSource {
-    fun getMovieList(category: String): Flowable<ApiResponse<List<Movie>>>
+    fun getMovieList(category: String): Flowable<ApiResponse<List<MovieResponse>>>
 }
