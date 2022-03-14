a03c54070ae7862d299ffdc5dc086bf02042637f
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-14 21:28:43 +0700

    Add LiveDataReactiveStream to convert flowable to livedata from publisher

--- app/src/main/java/id/kodesumsi/netwatch/ui/main/home/HomeFragmentViewModel.kt
+++ app/src/main/java/id/kodesumsi/netwatch/ui/main/home/HomeFragmentViewModel.kt
@@ -1,7 +1,9 @@
 package id.kodesumsi.netwatch.ui.main.home
 
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import id.kodesumsi.netwatch.core.data.source.network.NetworkConstant.Companion.POPULAR
 import id.kodesumsi.netwatch.core.domain.usecase.GetMovieListUseCase
 import javax.inject.Inject
 
@@ -10,8 +12,6 @@ class HomeFragmentViewModel @Inject constructor(
     private val moviewUseCase: GetMovieListUseCase
 ): ViewModel() {
 
-    fun getMovieList(category: String) {
-        moviewUseCase.getMovieList(category)
-    }
+    val popularMovies = LiveDataReactiveStreams.fromPublisher(moviewUseCase.getMovieList(POPULAR))
 
 }
