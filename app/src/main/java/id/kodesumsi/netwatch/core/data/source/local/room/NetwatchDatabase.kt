b083ae6f71c5821373d5492e145b7417f5d18f0c
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-15 19:51:52 +0700

    Add instance for room database

---
+++ app/src/main/java/id/kodesumsi/netwatch/core/data/source/local/room/NetwatchDatabase.kt
@@ -0,0 +1,34 @@
+package id.kodesumsi.netwatch.core.data.source.local.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import id.kodesumsi.netwatch.core.data.source.local.entity.MovieEntity
+import id.kodesumsi.netwatch.core.utils.Constant.Companion.DATABASE_NAME
+import id.kodesumsi.netwatch.core.utils.Constant.Companion.DATABASE_VERSION
+
+@Database(entities = [MovieEntity::class], version = DATABASE_VERSION, exportSchema = false)
+abstract class NetwatchDatabase: RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NetwatchDatabase? = null
+
+        fun getInstance(context: Context): NetwatchDatabase =
+            INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NetwatchDatabase::class.java,
+                    DATABASE_NAME
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+    }
+
+}
