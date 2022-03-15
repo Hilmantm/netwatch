151d5f56320cc78a188b5ceb4e4c2d7a13c10108
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-15 20:21:53 +0700

    Add moviedao function

--- app/src/main/java/id/kodesumsi/netwatch/core/data/source/local/room/MovieDao.kt
+++ app/src/main/java/id/kodesumsi/netwatch/core/data/source/local/room/MovieDao.kt
@@ -1,7 +1,24 @@
 package id.kodesumsi.netwatch.core.data.source.local.room
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import id.kodesumsi.netwatch.core.data.source.local.entity.MovieEntity
+import id.kodesumsi.netwatch.core.utils.Constant.Companion.MOVIE_TABLE
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
 
 @Dao
 interface MovieDao {
+
+    @Query("SELECT * FROM $MOVIE_TABLE")
+    fun getAllFavoriteMovies(): Flowable<List<MovieEntity>>
+
+    @Insert
+    fun insertFavoriteMovie(movie: MovieEntity): Completable
+
+    @Delete
+    fun removeFavoriteMovie(movie: MovieEntity): Completable
+
 }
