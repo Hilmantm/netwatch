a291840733b4dbb335dce6ce265a71943ecbe601
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-15 19:51:40 +0700

    Add movie dao and movie entity for local database

---
+++ app/src/main/java/id/kodesumsi/netwatch/core/data/source/local/entity/MovieEntity.kt
@@ -0,0 +1,33 @@
+package id.kodesumsi.netwatch.core.data.source.local.entity
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import id.kodesumsi.netwatch.core.utils.Constant.Companion.MOVIE_TABLE
+
+@Entity(tableName = MOVIE_TABLE)
+data class MovieEntity(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
+    @ColumnInfo(name = "overview")
+    val overview: String? = null,
+
+    @ColumnInfo(name = "title")
+    val title: String? = null,
+
+    @ColumnInfo(name = "posterPath")
+    val posterPath: String? = null,
+
+    @ColumnInfo(name = "backdropPath")
+    val backdropPath: String? = null,
+
+    @ColumnInfo(name = "releaseDate")
+    val releaseDate: String? = null,
+
+    @ColumnInfo(name = "voteAverage")
+    val voteAverage: Double? = null
+)
---
+++ app/src/main/java/id/kodesumsi/netwatch/core/data/source/local/room/MovieDao.kt
@@ -0,0 +1,7 @@
+package id.kodesumsi.netwatch.core.data.source.local.room
+
+import androidx.room.Dao
+
+@Dao
+interface MovieDao {
+}
