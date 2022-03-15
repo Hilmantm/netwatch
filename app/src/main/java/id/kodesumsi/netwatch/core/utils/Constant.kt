5378d17ac690ca5b2d21544d0623e4e413c6e99d
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-15 19:50:57 +0700

    Add constant for database name, version, and movie table name

--- app/src/main/java/id/kodesumsi/netwatch/core/utils/Constant.kt
+++ app/src/main/java/id/kodesumsi/netwatch/core/utils/Constant.kt
@@ -4,6 +4,9 @@ class Constant {
 
     companion object {
         const val SPLASHSCREEN_DELAY = 1000L
+        const val DATABASE_NAME = "Netwatch.db"
+        const val DATABASE_VERSION = 1
+        const val MOVIE_TABLE = "movies"
     }
 
 }
