01d29db02d9d839a3d3f93177d4fe6dbf65f98dc
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-03-21 19:41:30 +0700

    Error handle for dynamic feature

--- app/src/main/java/id/kodesumsi/netwatch/ui/main/MainActivity.kt
+++ app/src/main/java/id/kodesumsi/netwatch/ui/main/MainActivity.kt
@@ -1,16 +1,22 @@
 package id.kodesumsi.netwatch.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import dagger.hilt.android.AndroidEntryPoint
 import id.kodesumsi.netwatch.R
 import id.kodesumsi.netwatch.base.BaseActivity
 import id.kodesumsi.netwatch.databinding.ActivityMainBinding
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -18,6 +24,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun setupViewBinding(): (LayoutInflater) -> ActivityMainBinding = ActivityMainBinding::inflate
 
     override fun setupViewInstance(savedInstanceState: Bundle?) {
+        Log.d("MainActivity", "setupViewInstance: MODULE NAME => ${getString(R.string.favorite).lowercase(Locale.getDefault())}")
+
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val request = SplitInstallRequest.newBuilder()
+            .addModule(getString(R.string.favorite_module))
+            .build()
+        splitInstallManager.startInstall(request)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Success install", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                binding.bnvMain.setOnItemSelectedListener { menuitem ->
+                    val id = menuitem.itemId
+                    if(id == R.id.home_fragment) {
+                        Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+                        true
+                    } else {
+                        false
+                    }
+                }
+                Log.e("MainActivity", "splitInstallManager: ${it.message.toString()}", )
+            }
+
         val navController = findNavController(R.id.main_fragment_container_view)
         binding.bnvMain.setupWithNavController(navController)
     }
